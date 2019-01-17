c5d60c337b10b92ce9d04568b83432cc5c08d0f7
Javier Ventura <jventura1097@gmail.com>
2019-01-16 21:11:18 -0800

    added friend objects and finished survey page

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,31 @@
-var friends = [
+//Array of Objects with Possible Matches for Friends 
+//Photo is Keyword to Hash at RoboHash for Image
+var friendsArr = [
     {
-        "name": "",
-        "photo": "",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-
+        "name": "Johnny",
+        "photo": "world",
+        "scores": [4,2,1,5,2,1,2,3,4,2]
     },
     {
-
-    }
+        "name": "Che",
+        "photo": "friend",
+        "scores": [1,3,3,2,4,4,1,4,3,4]
+    },
+    {
+        "name": "Isabela",
+        "photo": '1',
+        "scores": [2,4,5,1,1,5,2,3,2,3]
+    },
+    {
+        "name": "Pamela",
+        "photo": "robot",
+        "scores": [3,5,4,3,3,3,5,1,1,2]
+    },
+    {
+        "name": "Rodrigo",
+        "photo": 'cool',
+        "scores": [5,1,2,4,3,2,4,3,5,1]
+    },
 ];
 
-module.exports = friends;
+module.exports = friendsArr;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,31 @@
-var friendData=("../data/friends");
+var friendsData=require("../data/friends");
 
 module.exports=function(app){
     app.post("/api/friends",function(req,res){
-        //Push New Survey Submition in Friend Data Array 
-        friendData.push(req.body);
+        //Push New Survey Submission in Friend Data Array 
+        friendsData.push(req.body);
 
+        var matched=friendsData[0];
+        var totDif=0;
+        var lowest=100;
+
+        //Loop Through Friends to Find Match
+        for(var i=0;i<friendsData.length-1;i++){
+            totDif=0;
+            //Loop Through Current Possible Friend and Users Scores to Get Total Difference
+            for(var j=0;j<10;j++){
+                let diff=Math.abs(req.body.scores[j]-friendsData[i].scores[j]);
+
+                totDif+=diff;
+            }
+
+            //If Total Difference is Lower Than Current Lowest Then Current Possible Friend Becomes the matched
+            if(totDif<lowest){
+                matched=friendsData[i];
+                lowest=totDif;
+            }
+        }
+
+        res.json(matched);
     });
 };
